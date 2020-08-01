94765564376a564eb274d26f7a7b9bdeb774e851
Kutlu Sahin <kutlugsahin@gmail.com>
2020-08-01 20:01:01 +0200

    dsads

--- build.js
+++ build.js
@@ -2,44 +2,110 @@ const fs = require('fs');
 const path = require('path');
 const package = fs.readFileSync('./package.json');
 const { exec } = require('child_process');
-
-const packageJson = JSON.parse(package);
-
-const projectDirs = packageJson.workspaces;
+const readline = require('readline');
 
 const argsSet = new Set(process.argv);
 
-
-projectDirs.forEach(dir => {
-    if (argsSet.has('inc')) {
-        const conf = getProjectPackageJson(dir);
-        const [major, minor, patch] = conf.version.split('.');
-        conf.version = `${major}.${minor}.${(+patch) + 1}`;
-        savePackageJson(dir, conf);
-    }
-});
+const packageJson = JSON.parse(package);
+const projectDirs = packageJson.workspaces;
+const projectNames = ['core', 'react', 'store'];
+const scopeName = '@re-active';
 
 
-const projects = {
-    
-}
+const projects = projectDirs.filter(p => projectNames.some(name => p.indexOf(`/${name}`))).reduce((acc, dir) => {
+    const name = dir.split('/')[1];
+    const folderPath = path.join(__dirname, dir);
+    const packageJsonPath = path.join(folderPath, 'package.json');
 
-function getProjectPackageJson(dir) {
-    const config = path.join(__dirname, dir, 'package.json');
-    console.log(config);
-    const configFile = fs.readFileSync(config);
-    return JSON.parse(configFile);
-}
+    acc[name] = {
+        folderPath,
+        packageName: `${scopeName}/${name}`,
+        packageJsonPath,
+        package: JSON.parse(fs.readFileSync(packageJsonPath)),
+        changed: false,
+    };
 
-function savePackageJson(dir, config) {
-    const packageJson = path.join(__dirname, dir, 'package.json');
-    fs.writeFileSync(packageJson, JSON.stringify(config, null, '\t'));
-}
+    return acc;
+}, {});
 
-function build(dir) {
-    const projectPath = path.join(__dirname, dir);
-    exec(`cd ${projectPath} && npm run build`, (err, stdout) => {
+function build(name) {
+    const { folderPath } = projects[name];
+    exec(`cd ${folderPath} && npm run build`, (err, stdout) => {
         console.log(stdout);
     });
+}
 
+function increment(name) {
+    const [major, minor, patch] = projects[name].version.split('.');
+    const newVersion = `${major}.${minor}.${(+patch) + 1}`;
+
+    projects[name].package.version = newVersion;
+    projects[name].changed = true;
+    const { packageName } = projects[name];
+
+    projectNames.forEach(p => {
+        const project = projects[p];
+        const { package } = project;
+
+        if (packageName in package.dependencies) {
+            package.dependencies[packageName] = newVersion;
+            project.changed = true;
+        }
+
+        if (packageName in package.devDependencies) {
+            package.devDependencies[packageName] = newVersion;
+            project.changed = true;
+        }
+    })
+}
+
+function publish(name) {
+    increment(name);
+    commit();
+
+    projectNames.forEach(p => {
+        exec(`cd ${projects[p].folderPath} && npm publish --access=public`, (err, stdout) => {
+            console.log(stdout);
+        });
+    })
+}
+
+function saveProject(name) {
+    const project = projects[name];
+
+    if (project.changed) {
+        fs.writeFileSync(project.packageJsonPath, JSON.stringify(project.package, null, '\t'));
+    }
+}
+
+function commit() {
+    projectNames.forEach(saveProject);
+}
+
+
+const reader = readline.createInterface({
+    input: process.stdin,
+})
+
+if (process.argv[2] === 'build') {
+    projectNames.forEach(build);
+    process.exit(0);
+}
+
+if (process.argv[2] === 'publish') {
+    const name = process.argv[3];
+
+    if (projectNames.includes(name)) {
+        publish(name);
+    } else {
+        console.log('project name should be one of ' + projectNames.join(' | '));
+    }
+    process.exit(0);
+}
+
+if (process.argv.length === 2) {
+    console.log('Listening commands...');
+    reader.on('line', (text) => {
+
+    })
 }
