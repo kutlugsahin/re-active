e2b53bda41cdf37f05714bd35bf3fe1a4da0b6b0
Kutlu Sahin <kutlugsahin@gmail.com>
2020-08-11 13:44:04 +0200

    rename

--- packages/app/main.ts
+++ packages/app/main.ts
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { App } from './src/demo/App';
 import { List } from './src/vue-reactivity';
-import { App as Componentdemo } from './src/componentdemo';
+import { App as Componentdemo } from './src/browsing';
 ReactDom.render(React.createElement(Componentdemo), document.getElementById('app'));
 
