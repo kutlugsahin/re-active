754f6d46fc820438894a066395c0e9a2a1e97e76
ksahin <kutlu.sahin@booking.com>
2020-08-30 15:58:30 +0300

    redux demo fix

--- packages/app/main-redux.ts
+++ packages/app/main-redux.ts
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { App } from './src/demo/App';
-import { List } from './src/vue-reactivity';
 import { App as Browser } from './src/browsing';
 import { App as BrowserRedux } from './src/browsing-redux';
 ReactDom.render(React.createElement(BrowserRedux), document.getElementById('app'));
