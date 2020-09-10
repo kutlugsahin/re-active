cca8b49662b9542addef7875367841b085b1e8cd
Kutlu Sahin <kutlugsahin@gmail.com>
2020-09-10 20:41:35 +0200

    observer demo updated

--- packages/app/src/browsing-observer/components/tree.tsx
+++ packages/app/src/browsing-observer/components/tree.tsx
@@ -80,6 +80,49 @@ export const TreeNode = observer((props: TreeNodeProps) => {
     )
 });
 
+// export class TreeNode extends ObserverComponent<TreeNodeProps> {
+//     onNodeExpand = (e: React.MouseEvent) => {
+//         e.stopPropagation();
+//         this.props.onExpand(this.props.node);
+//     }
+
+//     drawStateIndicator = ({ loading, expanded, children }: Node) => {
+//         if (loading) return <Loading />;
+//         if (children) return <span onClick={this.onNodeExpand} className={expanded ? 'caret expanded' : 'caret'}>▶</span>
+//         return <span className="caret" />;
+//     }
+
+//     renderTreeNode = (node: Node) => {
+//         return (
+//             <TreeNode
+//                 {...this.props}
+//                 key={node.id}
+//                 node={node}
+//                 level={this.props.level + 1}
+//             />
+//         );
+//     }
+
+//     render() {
+//         const { renderNode, node, onClick } = this.props;
+//         const { selected, expanded, children } = this.props.node;
+//         return <div className="nodecontainer">
+//             <div
+//                 id={`node-${node.id}`}
+//                 className={selected ? 'node selected' : 'node'}
+//                 onClick={() => onClick(node)}
+//                 onDoubleClick={this.onNodeExpand}
+//             >
+//                 {this.drawStateIndicator(node)}
+//                 {renderNode(node)}
+//             </div>
+//             <div style={{ display: expanded ? 'block' : 'none' }}>
+//                 {children?.map(this.renderTreeNode)}
+//             </div>
+//         </div>
+//     }
+// };
+
 
 export const Loading = () => {
     return <span className="loadingindicator">◠</span>
